8b0d0561688128c19632d8d26aab4300db61ead4
Shelby-Fulton <shelby.fulton7@gmail.com>
2024-10-21 22:16:02 -0400

    Initial Commit

---
+++ heartbeatzapp Watch App/heartbeatzappApp.swift
@@ -0,0 +1,17 @@
+//
+//  heartbeatzappApp.swift
+//  heartbeatzapp Watch App
+//
+//  Created by Shelby Fulton on 10/21/24.
+//
+
+import SwiftUI
+
+@main
+struct heartbeatzapp_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
