a61d0d2b568a44d084d5a102477b055e5ebd9088
Shelby-Fulton <shelby.fulton7@gmail.com>
2024-10-21 22:30:17 -0400

    Working on simulation

---
+++ heartbeatzapp Watch App/Models/HeartRateModel.swift
@@ -0,0 +1,12 @@
+//
+//  HeartRateModel.swift
+//  heartbeatzapp Watch App
+//
+//  Created by Shelby Fulton on 10/21/24.
+//
+
+import Foundation
+
+struct HeartRateModel {
+    var heartRate: Double
+}
--- heartbeatzapp Watch App/heartbeatzappApp.swift
+++ heartbeatzapp Watch App/heartbeatzappApp.swift
@@ -9,9 +9,19 @@ import SwiftUI
 
 @main
 struct heartbeatzapp_Watch_AppApp: App {
+    @StateObject private var healthKitManager = HealthKitManager()
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            if healthKitManager.isAuthorized {
+                HeartRateView()
+            } else {
+                Text("Requesting Health Data Access...")
+                    .onAppear {
+                        // Request authorization for HealthKit access when the app launches.
+                        healthKitManager.requestAuthorization()
+                    }
+            }
         }
     }
 }
